64b4860001be3f90a61968ea8f367c1c7f68c0ed
Phillip Chan <phil.chan@sce.com>
2021-08-25 15:13:27 -0700

    put up menu item

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,9 +11,9 @@ struct ContentView: View {
     var body: some View {
         TabView(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/,
                 content:  {
-                    Text("Tab Content 1").tabItem { Text("Tab Label 1") }.tag(1)
-                    Text("Tab Content 2").tabItem { Text("Tab Label 2") }.tag(2)
-                    Text("Tab Content 2").tabItem { Text("Tab Label 2") }.tag(3)
+                    Text("Plan").tabItem { Text("Plan") }.tag(1)
+                    Text("Shop").tabItem { Text("Shop") }.tag(2)
+                    Text("Meals").tabItem { Text("Meals") }.tag(3)
                 })
     }
 }
